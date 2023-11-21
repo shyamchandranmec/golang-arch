c462b3308d288aa4333d289c52f21f522288a557
Shyam Chandran <shyamchandranmec@gmail.com>
2023-11-21 14:14:29 +0530

    Adding hmac

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/sha512"
 	"encoding/base64"
 	"fmt"
 	"log"
@@ -12,8 +14,12 @@ type person struct {
 	First string
 }
 
-func main() {
+var key = [64]byte{}
 
+func main() {
+	for i := 0; i < 64; i++ {
+		key[i] = byte(i)
+	}
 	unp := "username:password"
 	encValue := base64.StdEncoding.EncodeToString([]byte(unp))
 	fmt.Println("Base 64 encode value is ", encValue)
@@ -40,7 +46,26 @@ func hashPassword(password string) ([]byte, error) {
 
 func comparePassword(password string, hp []byte) error {
 	if err := bcrypt.CompareHashAndPassword(hp, []byte(password)); err != nil {
-		return fmt.Errorf("Passwords do not match %w", err)
+		return fmt.Errorf("passwords do not match %w", err)
 	}
 	return nil
+}
+
+func signMessage(msg []byte) ([]byte, error) {
+	h := hmac.New(sha512.New, key[:])
+	_, err := h.Write(msg)
+	if err != nil {
+		return nil, fmt.Errorf("error while signing message in sign Message %w", err)
+	}
+	signature := h.Sum(nil)
+	return signature, nil
+}
+
+func checkSignature(msg, sig []byte) (bool, error) {
+	newSig, err := signMessage(msg)
+	if err != nil {
+		return false, fmt.Errorf("error in signing message in check Signature %w", err)
+	}
+	same := hmac.Equal(newSig, sig)
+	return same, nil
 }
