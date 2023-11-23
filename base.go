d6d3df593e0a68882d1297df3303128cdeadbbc4
Shyam Chandran <shyamchandranmec@gmail.com>
2023-11-23 15:42:19 +0530

    Adding ctr encryption

--- base.go
+++ base.go
@@ -1,35 +1,84 @@
 package main
 
 import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
 	"encoding/base64"
 	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
-	msg := "This is totally fun. /Thanks for helping me to learn this"
-	encoded := encode(msg)
-	fmt.Println(encoded)
-
-	// x := make([]byte, base64.URLEncoding.EncodedLen(len(msg)))
-	// base64.StdEncoding.Encode(x, []byte(msg))
-	// fmt.Print(string(x))
-	decoded, err := decode(encoded)
+	msg := "This is totally fun. Thanks for helping me to learn this"
+	key, err := getByteSlice("ilovedogs", 16)
 	if err != nil {
-		fmt.Println("Unable to decode ", encoded)
-	} else {
-		fmt.Println("Decode string is ", decoded)
+		fmt.Errorf("errorn in creating key %w", err)
+	}
+	fmt.Println("Key is ", base64.StdEncoding.EncodeToString(key))
+	result, iv, err := enDecode(key, nil, msg)
+	if err != nil {
+		fmt.Errorf("errorn in endecode msg %w", err)
+	}
+	fmt.Println("Before b64 Result is ", string(result))
+
+	eResult := base64.StdEncoding.EncodeToString(result)
+	fmt.Println("After b64 eResult is ", string(eResult))
+	fmt.Println()
+	fmt.Println()
+
+	dr, err := base64.StdEncoding.DecodeString(eResult)
+	if err != nil {
+		fmt.Errorf("errorn in decode dr %w", err)
 	}
 
-}
-
-func encode(msg string) string {
-	return base64.URLEncoding.EncodeToString([]byte(msg))
-}
-
-func decode(s string) (string, error) {
-	x, err := base64.URLEncoding.DecodeString(s)
+	dResult, _, err := enDecode(key, iv, string(dr))
 	if err != nil {
-		return "", fmt.Errorf("Unable to decode base 64 string %w", err)
+		fmt.Errorf("errorn in endecode  r2 %w", err)
 	}
-	return string(x), nil
+	fmt.Println("Decoded result is ", string(dResult))
+
 }
+
+func getByteSlice(s string, n int) ([]byte, error) {
+	password := s
+	b, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+	if err != nil {
+		return nil, fmt.Errorf("errorn in generating password %w", err)
+	}
+	key := b[:n]
+	return key, nil
+}
+
+func enDecode(key []byte, iv []byte, input string) ([]byte, []byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, nil, fmt.Errorf("Unable to create new cipher %w", err)
+	}
+	if iv == nil {
+		iv, err = getByteSlice("ivsaltstring", 16)
+	}
+
+	//iv := make([]byte, getByteSlice("ivsaltstring", 16))
+	fmt.Println("IV is ", base64.StdEncoding.EncodeToString(iv))
+	if err != nil {
+		return nil, nil, fmt.Errorf("Unable to create salt %w", err)
+	}
+
+	s := cipher.NewCTR(block, iv)
+	buff := &bytes.Buffer{}
+	sw := cipher.StreamWriter{
+		S: s,
+		W: buff,
+	}
+	_, err = sw.Write([]byte(input))
+	if err != nil {
+		return nil, nil, fmt.Errorf("Unable to SW write input %w", err)
+	}
+	output := buff.Bytes()
+	return output, iv, nil
+
+}
+
+func encrypt(key []byte, msg string)
