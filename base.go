3008afbab8b2f9dbda582f7fb00b1146b5229197
Shyam Chandran <shyamchandranmec@gmail.com>
2023-11-24 17:53:30 +0530

    Exploring sha256

--- base.go
+++ base.go
@@ -1,84 +1,85 @@
-package main
+// package main
 
-import (
-	"bytes"
-	"crypto/aes"
-	"crypto/cipher"
-	"encoding/base64"
-	"fmt"
+// import (
+// 	"bytes"
+// 	"crypto/aes"
+// 	"crypto/cipher"
+// 	"encoding/base64"
+// 	"fmt"
+// 
 
-	"golang.org/x/crypto/bcrypt"
-)
+// 	"golang.org/x/crypto/bcrypt"
+// )
 
-func main() {
-	msg := "This is totally fun. Thanks for helping me to learn this"
-	key, err := getByteSlice("ilovedogs", 16)
-	if err != nil {
-		fmt.Errorf("errorn in creating key %w", err)
-	}
-	fmt.Println("Key is ", base64.StdEncoding.EncodeToString(key))
-	result, iv, err := enDecode(key, nil, msg)
-	if err != nil {
-		fmt.Errorf("errorn in endecode msg %w", err)
-	}
-	fmt.Println("Before b64 Result is ", string(result))
+// func main() {
+// 	msg := "This is totally fun. Thanks for helping me to learn this"
+// 	key, err := getByteSlice("ilovedogs", 16)
+// 	if err != nil {
+// 		fmt.Errorf("errorn in creating key %w", err)
+// 	}
+// 	fmt.Println("Key is ", base64.StdEncoding.EncodeToString(key))
+// 	result, iv, err := enDecode(key, nil, msg)
+// 	if err != nil {
+// 		fmt.Errorf("errorn in endecode msg %w", err)
+// 	}
+// 	fmt.Println("Before b64 Result is ", string(result))
 
-	eResult := base64.StdEncoding.EncodeToString(result)
-	fmt.Println("After b64 eResult is ", string(eResult))
-	fmt.Println()
-	fmt.Println()
+// 	eResult := base64.StdEncoding.EncodeToString(result)
+// 	fmt.Println("After b64 eResult is ", string(eResult))
+// 	fmt.Println()
+// 	fmt.Println()
 
-	dr, err := base64.StdEncoding.DecodeString(eResult)
-	if err != nil {
-		fmt.Errorf("errorn in decode dr %w", err)
-	}
+// 	dr, err := base64.StdEncoding.DecodeString(eResult)
+// 	if err != nil {
+// 		fmt.Errorf("errorn in decode dr %w", err)
+// 	}
 
-	dResult, _, err := enDecode(key, iv, string(dr))
-	if err != nil {
-		fmt.Errorf("errorn in endecode  r2 %w", err)
-	}
-	fmt.Println("Decoded result is ", string(dResult))
+// 	dResult, _, err := enDecode(key, iv, string(dr))
+// 	if err != nil {
+// 		fmt.Errorf("errorn in endecode  r2 %w", err)
+// 	}
+// 	fmt.Println("Decoded result is ", string(dResult))
 
-}
+// }
 
-func getByteSlice(s string, n int) ([]byte, error) {
-	password := s
-	b, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
-	if err != nil {
-		return nil, fmt.Errorf("errorn in generating password %w", err)
-	}
-	key := b[:n]
-	return key, nil
-}
+// func getByteSlice(s string, n int) ([]byte, error) {
+// 	password := s
+// 	b, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("errorn in generating password %w", err)
+// 	}
+// 	key := b[:n]
+// 	return key, nil
+// }
 
-func enDecode(key []byte, iv []byte, input string) ([]byte, []byte, error) {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, nil, fmt.Errorf("Unable to create new cipher %w", err)
-	}
-	if iv == nil {
-		iv, err = getByteSlice("ivsaltstring", 16)
-	}
+// func enDecode(key []byte, iv []byte, input string) ([]byte, []byte, error) {
+// 	block, err := aes.NewCipher(key)
+// 	if err != nil {
+// 		return nil, nil, fmt.Errorf("Unable to create new cipher %w", err)
+// 	}
+// 	if iv == nil {
+// 		iv, err = getByteSlice("ivsaltstring", 16)
+// 	}
 
-	//iv := make([]byte, getByteSlice("ivsaltstring", 16))
-	fmt.Println("IV is ", base64.StdEncoding.EncodeToString(iv))
-	if err != nil {
-		return nil, nil, fmt.Errorf("Unable to create salt %w", err)
-	}
+// 	//iv := make([]byte, getByteSlice("ivsaltstring", 16))
+// 	fmt.Println("IV is ", base64.StdEncoding.EncodeToString(iv))
+// 	if err != nil {
+// 		return nil, nil, fmt.Errorf("Unable to create salt %w", err)
+// 	}
 
-	s := cipher.NewCTR(block, iv)
-	buff := &bytes.Buffer{}
-	sw := cipher.StreamWriter{
-		S: s,
-		W: buff,
-	}
-	_, err = sw.Write([]byte(input))
-	if err != nil {
-		return nil, nil, fmt.Errorf("Unable to SW write input %w", err)
-	}
-	output := buff.Bytes()
-	return output, iv, nil
+// 	s := cipher.NewCTR(block, iv)
+// 	buff := &bytes.Buffer{}
+// 	sw := cipher.StreamWriter{
+// 		S: s,
+// 		W: buff,
+// 	}
+// 	_, err = sw.Write([]byte(input))
+// 	if err != nil {
+// 		return nil, nil, fmt.Errorf("Unable to SW write input %w", err)
+// 	}
+// 	output := buff.Bytes()
+// 	return output, iv, nil
 
-}
+// }
 
-func encrypt(key []byte, msg string)
+// func encrypt(key []byte, msg string)
--- main.go
+++ main.go
@@ -1,160 +1,42 @@
-// package main
+package main
 
-// import (
-// 	"crypto/hmac"
-// 	"crypto/rand"
-// 	"crypto/sha512"
-// 	"encoding/base64"
-// 	"fmt"
-// 	"io"
-// 	"log"
-// 	"time"
-// 
-// 
+import (
+	"crypto/sha256"
+	"fmt"
+	"log"
+	"os"
+)
 
-// 	"github.com/Azure/azure-amqp-common-go/v3/uuid"
-// 	"github.com/golang-jwt/jwt/v4"
-// 	"github.com/google/uuid"
-// 	"golang.org/x/crypto/bcrypt"
-// )
+func main() {
+	file, err := os.Open("sample-file.txt")
+	if err != nil {
+		errx := fmt.Errorf("error in reading file %w", err)
+		log.Fatal(errx)
+	}
+	defer file.Close()
+	data := make([]byte, 100)
+	count, err := file.Read(data)
+	fmt.Printf("Read %d bytes:%q", count, data[:count])
+	fmt.Println()
+	h := sha256.New()
+	h.Write(data)
+	fmt.Printf("Here is the type Before sum %T", h)
+	fmt.Println()
+	xb := h.Sum(nil)
+	fmt.Printf("Here is the type after sum %T", xb)
+	fmt.Println()
+	fmt.Printf("xb is %x", xb)
+	fmt.Println()
 
-// type person struct {
-// 	First string
-// }
+	xb = h.Sum(nil)
+	fmt.Printf("2 Here is the type after sum %T", xb)
+	fmt.Println()
+	fmt.Printf("2 xb is %x", xb)
+	fmt.Println()
 
-// var key = [64]byte{}
-// var keys = map[string]Key{}
-// var currentKid = ""
-
-// type UserClaims struct {
-// 	jwt.RegisteredClaims
-// 	SessionId int
-// }
-
-// func (u *UserClaims) Valid() error {
-// 	if !u.VerifyExpiresAt(time.Now(), true) {
-// 		return fmt.Errorf("token has expired")
-// 	}
-// 	if u.SessionId == 0 {
-// 		return fmt.Errorf("invalid sessin id")
-// 	}
-// 	return nil
-// }
-
-// func main() {
-// 	for i := 0; i < 64; i++ {
-// 		key[i] = byte(i)
-// 	}
-// 	unp := "username:password"
-// 	encValue := base64.StdEncoding.EncodeToString([]byte(unp))
-// 	fmt.Println("Base 64 encode value is ", encValue)
-// 	password := "1234455"
-// 	bh, err := hashPassword(password)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	fmt.Println("hash password is ", string(bh))
-// 	if err = comparePassword(password, bh); err != nil {
-// 		log.Fatal("Not logged in ")
-// 	}
-
-// 	log.Println("Logged in")
-// 	msg := "Shyam Message"
-// 	smsg, _ := signMessage([]byte(msg))
-// 	fmt.Println("Signed message is ", base64.StdEncoding.EncodeToString(smsg))
-// 	same, _ := checkSignature([]byte(msg), smsg)
-// 	fmt.Println("Check signature is ", same)
-// }
-
-// func hashPassword(password string) ([]byte, error) {
-// 	bh, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("error while generation bcrypt password %w", err)
-// 	}
-// 	return bh, nil
-// }
-
-// func comparePassword(password string, hp []byte) error {
-// 	if err := bcrypt.CompareHashAndPassword(hp, []byte(password)); err != nil {
-// 		return fmt.Errorf("passwords do not match %w", err)
-// 	}
-// 	return nil
-// }
-
-// func signMessage(msg []byte) ([]byte, error) {
-// 	h := hmac.New(sha512.New, keys[currentKid].key)
-// 	_, err := h.Write(msg)
-// 	if err != nil {
-// 		return nil, fmt.Errorf("error while signing message in sign Message %w", err)
-// 	}
-// 	signature := h.Sum(nil)
-// 	return signature, nil
-// }
-
-// func checkSignature(msg, sig []byte) (bool, error) {
-// 	newSig, err := signMessage(msg)
-// 	if err != nil {
-// 		return false, fmt.Errorf("error in signing message in check Signature %w", err)
-// 	}
-// 	same := hmac.Equal(newSig, sig)
-// 	return same, nil
-// }
-
-// func CreateToken(c *UserClaims) (string, error) {
-// 	t := jwt.NewWithClaims(jwt.SigningMethodHS512, c)
-// 	signedToken, err := t.SignedString(keys[currentKid].key)
-// 	if err != nil {
-// 		return "", fmt.Errorf("error in create token when signing token")
-// 	}
-// 	return signedToken, nil
-// }
-
-// type Key struct {
-// 	key     []byte
-// 	created time.Time
-// }
-
-// // cron job that runs every hour
-// func generateNewKey() error {
-// 	newKey := make([]byte, 64)
-// 	_, err := io.ReadFull(rand.Reader, newKey)
-// 	if err != nil {
-// 		return fmt.Errorf("error in generating new key %w", err)
-// 	}
-// 	uid, err := uuid.NewV4()
-// 	if err != nil {
-// 		return fmt.Errorf("error in generating uuid %w", err)
-// 	}
-// 	keys[uid.String()] = Key{
-// 		key:     newKey,
-// 		created: time.Now(),
-// 	}
-// 	currentKid = uid.String()
-// 	return nil
-// }
-
-// func parseToken(token string) (*UserClaims, error) {
-// 	t, err := jwt.ParseWithClaims(token, &UserClaims{}, func(t *jwt.Token) (interface{}, error) {
-// 		if t.Method.Alg() != jwt.SigningMethodHS512.Alg() {
-// 			return nil, fmt.Errorf("invalid signing algorith")
-// 		}
-// 		kid, ok := t.Header["kid"].(string)
-// 		if !ok {
-// 			return nil, fmt.Errorf("Invalid key id")
-// 		}
-// 		k, ok := keys[kid]
-
-// 		if !ok {
-// 			return nil, fmt.Errorf("Invalid key id")
-// 		}
-// 		return k, nil
-// 	})
-// 	if err != nil {
-// 		return nil, fmt.Errorf("error in parsing token %w", err)
-// 	}
-// 	if !t.Valid {
-// 		return nil, fmt.Errorf("invalid token")
-// 	}
-// 	claims := t.Claims.(*UserClaims)
-// 	return claims, nil
-// }
+	xb = h.Sum(xb)
+	fmt.Printf("3 Here is the type after sum %T", xb)
+	fmt.Println()
+	fmt.Printf("3 xb is %x", xb)
+	fmt.Println()
+}
